fa1fed4640ee48abc19a932e2c0605d14315dd82
Matthew <matthewegan001@gmail.com>
2016-08-24 14:12:40 -0400

    player movement and camera controls

--- Roll a Ball Online/Assets/Scripts/CameraController.cs
+++ Roll a Ball Online/Assets/Scripts/CameraController.cs
@@ -6,16 +6,22 @@ public class CameraController : MonoBehaviour
 {
 
     public GameObject target;
-    public float rotateSpeed = 5.0f;
-    public float ZoomSpeed = 10f;
-    public float maxZoomDistance = 90f;
+    public float rotateSpeed;
+    public float zoomSpeed;
+    public float maxZoomDistance;
     public float minZoomDistance = 15f;
     private Vector3 offset;
-    public float cameraDistance = 10;
+    //private Vector3 newCameraPosition;
 
     // Use this for initialization
     void Start()
     {
+        //newCameraPosition = Camera.main.transform.position;
+        rotateSpeed = 5.0f;
+        zoomSpeed = 1f;
+        maxZoomDistance = 90f;
+        minZoomDistance = 15f;
+
         offset = transform.position - target.transform.position;
         transform.LookAt(target.transform);
     }
@@ -24,35 +30,76 @@ public class CameraController : MonoBehaviour
     // guarneteed to run after all items have been processed in Update
     void LateUpdate()
     {
+        // Enable the camera to move with the target
+        positionHandler();
 
-        // Place the camera in a position relative to the target
+        // Enable camera zoom on our target
+        zoomHandler();
+
+        // Enable camera rotation on the X axis
+        xRotationHandler();
+
+        // Enable camera rotation on the Y axis
+        yRotationHandler();
+    }
+
+    void positionHandler()
+    {
+        // Move the camera relative to the target
         transform.position = target.transform.position + offset;
+    }
 
+    void yRotationHandler()
+    {
+        // TODO: Limit the camera angle
+
+        // If the right mouse button is pressed
+        if (Input.GetMouseButton(1))
+        {
+            // Allow for rotation on the Y axis (up and down)
+            transform.RotateAround(target.transform.position, transform.right * -1, Input.GetAxis("Mouse Y") * rotateSpeed);
+
+            // Caclulate the new camera direction/position relative to our target
+            offset = transform.position - target.transform.position;
+        }
+    }
+
+    void xRotationHandler()
+    {
         // If the right mouse button is pressed
         if (Input.GetMouseButton(1))
         {
             // Allow for rotation on the X axis (left and right)
             transform.RotateAround(target.transform.position, Vector3.up, Input.GetAxis("Mouse X") * rotateSpeed);
-            // TODO: Camera rotates 180 degrees on the y axis when at the limits of the RotateAround
-            // TODO: Limit the camera angle
-            // Allow for rotation on the Y axis (up and down)
-            transform.RotateAround(target.transform.position, transform.right * -1, Input.GetAxis("Mouse Y") * rotateSpeed);
-            // Caclulate a new camera position relative to our target
+
+            // Caclulate the new camera direction/position relative to our target
+            offset = transform.position - target.transform.position;
+        }
+    }
+
+    void zoomHandler()
+    {
+        //TODO: Smooth zoom
+        //TODO: limit zoom depths
+
+        // Get any potential data caused by the scroll wheel
+        float scrollEventData = Input.GetAxis("Mouse ScrollWheel");
+
+        // If scrollwheel data exists
+        if (scrollEventData != 0)
+        {
+            Vector3 slope = offset * Mathf.Sign(scrollEventData) * -1;
+            slope.Normalize();
+            Vector3 newCameraPosition = Camera.main.transform.position;
+            //zoomSpeed = 50;
+            newCameraPosition += (slope * zoomSpeed);
+            Camera.main.transform.position = newCameraPosition;
+            //Vector3.Lerp(Camera.main.transform.position, newCameraPosition, Time.deltaTime * 2);
             offset = transform.position - target.transform.position;
         }
 
-        //Acquire the camera's current field of view value
-        float fov = Camera.main.fieldOfView;
-        //Adjust the field of view determined by the mouse scroll wheel
-        //Multiple by -1 to invert scroll direction
-        fov += Input.GetAxis("Mouse ScrollWheel") * ZoomSpeed * -1;
-        //Limit how close or far we can zoom
-        fov = Mathf.Clamp(fov, minZoomDistance, maxZoomDistance);
-        //Set the new field of view for the camera
-        Camera.main.fieldOfView = fov;
-
-
-        // Now that the camera has been moved, point it towards the target
-        transform.LookAt(target.transform);
+        //Debug.Log(Camera.main.transform.position);
+        //Debug.Log(newCameraPosition);
+        //Vector3.Lerp(Camera.main.transform.position, newCameraPosition, Time.deltaTime * 2);
     }
 }
--- Roll a Ball Online/Assets/Scripts/PlayerController.cs
+++ Roll a Ball Online/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour {
     public float speed;
     private Rigidbody rb;
     private int count;
+    public Camera playerCamera;
 
     //called on the first frame that the script is active
     void Start()
@@ -26,12 +27,7 @@ public class PlayerController : MonoBehaviour {
     //this is where our physics code will go.
     void FixedUpdate ()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-
-        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
-
-        rb.AddForce(movement * speed);
+        movePlayer();
     }
 
     void OnTriggerEnter(Collider other)
@@ -43,4 +39,47 @@ public class PlayerController : MonoBehaviour {
             count = count + 1;
         }
     }
+
+    void movePlayer()
+    {
+        // Obtain data from key presses
+        float horizontalKeyData = Input.GetAxis("Horizontal");
+        float VerticalKeyData = Input.GetAxis("Vertical");
+
+        // Force to the applied on our player
+        Vector3 horizontalMovement = new Vector3();
+        Vector3 verticalMovement = new Vector3();
+
+        // Camera's reletaive position
+        Vector3 offset = transform.position - playerCamera.transform.position;
+
+        // If we have horizontal data, we are strafing (left or right)
+        if (horizontalKeyData != 0)
+        {
+            // Create a force perpendicular to the camera face
+            horizontalMovement = new Vector3(offset.z, 0.0f, offset.x * -1);
+
+            // Ensure the direction of the force is in the desired direction
+            horizontalMovement.x *= Mathf.Sign(horizontalKeyData);
+            horizontalMovement.z *= Mathf.Sign(horizontalKeyData);
+        }
+
+        // If we have vertical data, we are moving forward or backward
+        if (VerticalKeyData != 0)
+        {
+            // Create a force parallel to the camera face
+            verticalMovement = new Vector3(offset.x, 0.0f, offset.z);
+            // Ensure the direction of the force is in the desired direction
+            verticalMovement.x *= Mathf.Sign(VerticalKeyData);
+            verticalMovement.z *= Mathf.Sign(VerticalKeyData);
+        }
+
+        // Sum the forces from our inputs and normalize it
+        Vector3 movement = horizontalMovement + verticalMovement;
+        movement.Normalize();
+
+        // Apply the force to our player
+        rb.AddForce(movement * speed);
+    }
+
 }
